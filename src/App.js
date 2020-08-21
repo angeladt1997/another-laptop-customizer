824f706e551612252df437f36ac4cd4ed701b80f
Angela Thomas <66443127+angeladt1997@users.noreply.github.com>
2020-08-21 16:44:53 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Selected from './Selected';
+import './App.css';
+import Cart from './cart';
+import CustomizeForm from './CustomizeForm';
+
+class App extends Component {
+  state = {
+    selected: Selected
+  };
+
+  updateFeature = (feature, newValue) => {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  };
+
+  render() {
+    const {selected} = this.state
+    return (
+      <div className="App">
+        <header>
+          <h1>ELF Computing | Laptops</h1>
+        </header>
+        <main>
+          <CustomizeForm 
+            selected={selected}
+            updateFeature={this.updateFeature}
+          />
+          <Cart 
+            selected={selected}
+          />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
